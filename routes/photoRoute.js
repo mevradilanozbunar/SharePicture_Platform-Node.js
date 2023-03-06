bbd71f40783848f6417010aa5905eba560bb330e
Mevra Dilan Erten Özbunar <112767383+mevradilanozbunar@users.noreply.github.com>
2023-03-06 10:49:20 +0300

    commit

--- routes/photoRoute.js
+++ routes/photoRoute.js
@@ -4,5 +4,5 @@ import * as photoController from '../controllers/photoController.js';
 const Photorouter = express.Router();
 
 Photorouter.route("/").post(photoController.createPhoto).get(photoController.getAllPhotos);
-
+Photorouter.route("/:id").get(photoController.getAPhoto);
 export default Photorouter;
