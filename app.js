42a6c47db4e79278d61e2506b853fed0709e580c
Mevra Dilan Erten Özbunar <112767383+mevradilanozbunar@users.noreply.github.com>
2023-03-07 15:58:25 +0300

    add checkUser

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import Pagerouter from './routes/pageRoute.js';
 import Photorouter from './routes/photoRoute.js';
 import Userrouter from './routes/userRoute.js';
 import cookieParser from 'cookie-parser';
+import checkUser from './middlewares/checkUser.js';
 
 dotenv.config();
 conn();
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));//Bu ara yazılım, istek nesnesindeki "req.body" özelliğine form verilerini ekler ve uygulamanın sonraki ara yazılımı veya yönlendirme işlevleri tarafından kullanılabilir hale getirir. Bu sayede, gelen isteklerdeki form verileri kolayca kullanılabilir hale gelir.
 app.use(cookieParser());
 
+app.use("*",checkUser); //tüm get methodlarınde checkuserı check etmeli
 app.use("/",Pagerouter);
 app.use("/photos",Photorouter);
 app.use("/users",Userrouter);
@@ -24,3 +26,5 @@ app.use("/users",Userrouter);
 app.listen(process.env.PORT,()=>{
     console.log(`server running on port ${process.env.PORT}`);
 });
+
+//app.use("*", checkUser) kodu, Express.js uygulamasında tüm HTTP isteklerini yakalamak ve checkUser isimli bir middleware fonksiyonu kullanarak her istek öncesi kimlik doğrulama yapmak için kullanılır.Yani, app.use("*", checkUser) kodu, tüm HTTP isteklerini karşılar ve checkUser isimli middleware fonksiyonunu her istek öncesi çağırarak kimlik doğrulama işlemini gerçekleştirir. Bu sayede, tüm route handler'larının öncesinde kimlik doğrulama işleminin gerçekleştirilmesi sağlanır.
---
+++ middlewares/checkUser.js
@@ -0,0 +1,27 @@
+import JsonWebToken from 'jsonwebtoken';
+
+const checkUser = async (req, res, next) => {
+    const token = req.cookies.JsonWebToken;
+  
+    if (token) {
+      jwt.verify(token, process.env.JWT_PRIVATE_KEY, async (err, decodedToken) => {
+        if (err) {
+          console.log(err.message);
+          res.locals.user = null;
+          next();
+        } else {
+          const user = await User.findById(decodedToken.userId);
+          res.locals.user = user;
+          //JWT decodedToken nesnesine ayrıştırılır ve kullanıcının kimliği (decodedToken.userId) kullanılarak veritabanından kullanıcı bilgileri (user) getirilir. Kullanıcı bilgileri res.locals.user değişkenine atanır ve bir sonraki middleware'e veya route handler'ına geçilir.
+          next();
+        }
+      });
+    } else {
+      res.locals.user = null;
+      next();
+    }
+  };
+
+  export default checkUser;
+
+  //res.locals özelliği, app.locals özelliğine benzer, ancak app.locals tüm uygulama yaşam döngüsü boyunca ortak olan verileri depolamak için kullanılırken, res.locals her HTTP isteği için ayrılan bir nesne olduğu için, her HTTP isteği için özelleştirilmiş verileri depolamak için kullanılır.
